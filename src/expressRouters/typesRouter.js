ab5adb32545866902f17f32f215ba0137fcd1c00
Jeff Haddock <jeff.haddock.developer@gmail.com>
2025-12-22 11:15:11 -0500

    adds type router

--- src/expressRouters/typesRouter.js
+++ src/expressRouters/typesRouter.js
@@ -10,18 +10,17 @@ router.get('/', (req, res) =>{
     .select('master_type_enums.type_name as enum_type', 'master_type_enum_items.item_name', 'master_type_enum_items.item_display_text', 'master_type_enum_items.item_additional_info', 'master_type_enum_items.item_value')
     .orderBy(['master_type_enums.id', 'master_type_enum_items.item_value'])
     .then(data => {
-        console.log(data)
-        let groupedData = {}
+        let groupedData = {};
         data.forEach(row => {
             if (!groupedData[row.enum_type]) {
-                groupedData[row.enum_type] = []
+                groupedData[row.enum_type] = [];
             }
             groupedData[row.enum_type].push({
                 item_name: row.item_name,
                 item_display_text: row.item_display_text,
                 item_additional_info: row.item_additional_info,
                 item_value: row.item_value
-            })
+            });
         })
         
         res.json(groupedData)
